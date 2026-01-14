4d6f10fbb5809e88e8807a68d83166d1ddb04a97
miho <paglinawan0520@gmail.com>
2026-01-14 21:55:38 +0900

    update: - 区切り線の太さの調整 - シングル・コレクションラベルの追加 - 詳細の表示切り替え機能の追加

--- widget-src/code.tsx
+++ widget-src/code.tsx
@@ -1,7 +1,13 @@
 const { widget } = figma;
 const { useSyncedState, usePropertyMenu, AutoLayout, Text, Input, SVG } =
   widget;
-import { propertyIcon, actionIcon, closeIcon } from "./icons";
+import {
+  propertyIcon,
+  actionIcon,
+  eyeIcon,
+  eyeOffIcon,
+  closeIcon,
+} from "./icons";
 
 function Widget() {
   const [objectName, setObjectName] = useSyncedState(
@@ -12,6 +18,11 @@ function Widget() {
     "プロパティ",
   ]);
   const [actions, setActions] = useSyncedState("actions", ["アクション"]);
+  const [type, setType] = useSyncedState("type", "シングル");
+  const [isDisplayDetail, setIsDisplayDetail] = useSyncedState(
+    "isDisplayDetail",
+    true
+  );
 
   usePropertyMenu(
     [
@@ -27,12 +38,20 @@ function Widget() {
         tooltip: "アクションを追加",
         icon: actionIcon,
       },
+      {
+        itemType: "action",
+        propertyName: "toggle-display",
+        tooltip: isDisplayDetail ? "詳細を隠す" : "詳細を表示",
+        icon: isDisplayDetail ? eyeOffIcon : eyeIcon,
+      },
     ],
     (e) => {
       if (e.propertyName === "add-property") {
         setProperties([...properties, `プロパティ${properties.length + 1}`]);
       } else if (e.propertyName === "add-action") {
         setActions([...actions, `アクション${actions.length + 1}`]);
+      } else if (e.propertyName === "toggle-display") {
+        setIsDisplayDetail(!isDisplayDetail);
       }
     }
   );
@@ -45,112 +64,125 @@ function Widget() {
       cornerRadius={20}
       fill="#fff"
       stroke="#CCCCCC"
-      width={320}
+      width={280}
     >
       {/* オブジェクト名エリア */}
       <AutoLayout
-        padding={{ vertical: 16, horizontal: 16 }}
-        stroke="#CCCCCC"
+        padding={{ top: 12, bottom: 16, left: 16, right: 16 }}
         width="fill-parent"
+        direction="vertical"
+        spacing={8}
       >
+        <AutoLayout
+          padding={4}
+          fill="#eeeeee"
+          cornerRadius={4}
+          onClick={() =>
+            setType(type === "シングル" ? "コレクション" : "シングル")
+          }
+          horizontalAlignItems="center"
+        >
+          <Text fontSize={10} fill="#999999" fontWeight={700}>
+            {type}
+          </Text>
+        </AutoLayout>
         <Input
           value={objectName}
-          fontSize={12}
+          fontSize={14}
           fontWeight={700}
           width="fill-parent"
           onTextEditEnd={(event) => setObjectName(event.characters)}
           inputBehavior="truncate"
         />
       </AutoLayout>
-      {/* 区切り線 */}
-      <AutoLayout height={0.5} fill="#CCCCCC" width="fill-parent" />
-
-      {/* プロパティリスト */}
-      {properties.length > 0 && (
-        <AutoLayout direction="vertical" width="fill-parent">
-          {properties.map((prop, i) => (
-            <>
-              {i > 0 && (
+      {/* プロパティ・アクションリスト（詳細表示時のみ） */}
+      {isDisplayDetail && properties.length > 0 && (
+        <>
+          <AutoLayout height={2} fill="#CCCCCC" width="fill-parent" />
+          <AutoLayout direction="vertical" width="fill-parent">
+            {properties.map((prop, i) => (
+              <>
+                {i > 0 && (
+                  <AutoLayout
+                    height={0.5}
+                    width="fill-parent"
+                    stroke="#CCCCCC"
+                    strokeDashPattern={[2, 2]}
+                  />
+                )}
                 <AutoLayout
-                  height={0.5}
+                  key={i}
+                  padding={{ vertical: 12, horizontal: 16 }}
+                  spacing={8}
                   width="fill-parent"
-                  stroke="#CCCCCC"
-                  strokeDashPattern={[2, 2]}
-                />
-              )}
-              <AutoLayout
-                key={i}
-                padding={{ vertical: 12, horizontal: 16 }}
-                spacing={8}
-                width="fill-parent"
-                verticalAlignItems="center"
-              >
-                <Input
-                  value={prop}
-                  fontSize={12}
-                  width="fill-parent"
-                  onTextEditEnd={(v) => {
-                    const newProps = [...properties];
-                    newProps[i] = v.characters;
-                    setProperties(newProps);
-                  }}
-                  inputBehavior="truncate"
-                />
-                <SVG
-                  src={closeIcon}
-                  onClick={() => {
-                    setProperties(properties.filter((_, idx) => idx !== i));
-                  }}
-                />
-              </AutoLayout>
-            </>
-          ))}
-        </AutoLayout>
+                  verticalAlignItems="center"
+                >
+                  <Input
+                    value={prop}
+                    fontSize={12}
+                    width="fill-parent"
+                    onTextEditEnd={(v) => {
+                      const newProps = [...properties];
+                      newProps[i] = v.characters;
+                      setProperties(newProps);
+                    }}
+                    inputBehavior="truncate"
+                  />
+                  <SVG
+                    src={closeIcon}
+                    onClick={() => {
+                      setProperties(properties.filter((_, idx) => idx !== i));
+                    }}
+                  />
+                </AutoLayout>
+              </>
+            ))}
+          </AutoLayout>
+        </>
       )}
-      {/* 区切り線 */}
-      <AutoLayout height={0.5} fill="#CCCCCC" width="fill-parent" />
-
-      {/* アクションリスト */}
-      {actions.length > 0 && (
-        <AutoLayout direction="vertical" width="fill-parent">
-          {actions.map((action, i) => (
-            <>
-              {i > 0 && (
+      {isDisplayDetail && actions.length > 0 && (
+        <>
+          <AutoLayout height={1} fill="#CCCCCC" width="fill-parent" />
+          <AutoLayout direction="vertical" width="fill-parent">
+            {actions.map((action, i) => (
+              <>
+                {i > 0 && (
+                  <AutoLayout
+                    height={0.5}
+                    width="fill-parent"
+                    stroke="#CCCCCC"
+                    strokeDashPattern={[2, 2]}
+                  />
+                )}
                 <AutoLayout
-                  height={0.5}
+                  key={i}
+                  padding={{ vertical: 12, horizontal: 16 }}
+                  spacing={8}
                   width="fill-parent"
-                  stroke="#CCCCCC"
-                  strokeDashPattern={[2, 2]}
-                />
-              )}
-              <AutoLayout
-                key={i}
-                padding={{ vertical: 12, horizontal: 16 }}
-                spacing={8}
-                width="fill-parent"
-                verticalAlignItems="center"
-              >
-                <Input
-                  value={action}
-                  fontSize={12}
-                  width="fill-parent"
-                  onTextEditEnd={(v) => {
-                    const newActions = [...actions];
-                    newActions[i] = v.characters;
-                    setActions(newActions);
-                  }}
-                  inputBehavior="truncate"
-                />
-                <SVG
-                  src={closeIcon}
-                  onClick={() => {
-                    setActions(actions.filter((_, idx) => idx !== i));
-                  }}
-                />
-              </AutoLayout>
-            </>
-          ))}
-        </AutoLayout>
+                  verticalAlignItems="center"
+                >
+                  <Input
+                    value={action}
+                    fontSize={12}
+                    width="fill-parent"
+                    onTextEditEnd={(v) => {
+                      const newActions = [...actions];
+                      newActions[i] = v.characters;
+                      setActions(newActions);
+                    }}
+                    inputBehavior="truncate"
+                  />
+                  <SVG
+                    src={closeIcon}
+                    onClick={() => {
+                      setActions(actions.filter((_, idx) => idx !== i));
+                    }}
+                  />
+                </AutoLayout>
+              </>
+            ))}
+          </AutoLayout>
+        </>
       )}
     </AutoLayout>
   );
